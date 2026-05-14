d76fa953ead482b78920ac5e5f2caffef87efd63
Ubuntu <ubuntu@ip-172-31-44-71.ec2.internal>
2026-05-14 13:38:14 +0000

    Removed frontend submodule issue

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Full Stack CI/CD Project </h1>
+      <p>Frontend running successfully</p>
+    </div>
+  );
+}
+
+export default App;
